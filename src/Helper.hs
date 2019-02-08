7595725d1c72f98b671623034c8b380fdf6bb249
Sasa Bogicevic <t4nt0r@protonmail.com>
2019-02-08 16:54:33 +0100

    Remove unnecessary functions

--- src/Helper.hs
+++ src/Helper.hs
@@ -1,7 +1,14 @@
-module Helper where
+module Helper
+  ( generateImage
+  , pos
+  , peek
+  , peeks
+  , seek
+  , seeks
+  , experiment
+  )
+  where
 
-import Data.List (sortBy)
-import Data.Ord (comparing)
 import qualified Data.Vector as V
 import Data.Word (Word8)
 import System.Random
@@ -30,15 +37,6 @@ seeks f (Store f' s) = Store f' (f s)
 -- Run an experiment in the store.
 experiment :: Functor f => (s -> f s) -> Store s a -> f a
 experiment f (Store s a) = fmap s (f a)
-
-mkRange :: [(Int, Int)]
-mkRange =
-  let x = [0..100]
-   in [ (a,b) | b <- reverse x,a <- x]
-
-generateCoordinates :: [Coord]
-generateCoordinates =
-  map (\(a, b) -> Coord a b) $ sortBy (comparing $ fst) mkRange
 
 randomWord8 :: IO [Word8]
 randomWord8 = do
