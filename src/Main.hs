c49a074dc1dc9bfea79883131dd85d2f8d18a0de
Sasa Bogicevic <t4nt0r@protonmail.com>
2019-02-14 22:24:27 +0100

    Display colors in terminal

--- src/Main.hs
+++ src/Main.hs
@@ -3,34 +3,53 @@ module Main where
 import Brick.AttrMap
 import Brick.Main
 import Brick.Types
+import Brick.Util
 import Brick.Widgets.Core
+import Brick.Widgets.Border
 import qualified Data.Vector as V
 import Graphics.Vty.Input.Events
+import Graphics.Vty.Attributes
 import Helper
 import Types
 
 type ResourceName = String
 
-app :: App (V.Vector (V.Vector RGB)) e ResourceName
-app =
+app :: [(AttrName, Attr)] -> App (V.Vector (V.Vector RGB)) e ResourceName
+app cMap =
   App
     { appDraw = drawTui
     , appChooseCursor = showFirstCursor
     , appHandleEvent = handleTuiEvent
     , appStartEvent = pure
-    , appAttrMap = const $ attrMap mempty []
+    , appAttrMap = const $ attrMap mempty cMap
     }
+
+colorMap :: V.Vector (V.Vector RGB) -> [(AttrName, Attr)]
+colorMap cmap =
+  concat $ V.toList $ V.map (\v -> V.toList $ V.map
+          (\(RGB a b c) ->
+           let color = rgbColor a b c
+            in (attrName (show color), fg color)
+          ) v
+        ) cmap
 
 buildInitialState :: IO (V.Vector (V.Vector RGB))
 buildInitialState = generateColors
 
 drawTui :: (V.Vector (V.Vector RGB)) -> [Widget ResourceName]
-drawTui image = [ vBox $ V.toList $ V.map drawCell image]
+drawTui i = [ border $ hBox $ concat $ V.map V.toList $ V.map drawCell i]
 
-drawCell :: V.Vector RGB -> Widget n
-drawCell v = str . show $ V.head v
+drawCell :: V.Vector RGB -> V.Vector (Widget n)
+drawCell v =
+  V.map (\(RGB a b c) ->
+    let color = rgbColor a b c
+        cStr  = show color
+     in (withAttr (attrName cStr)) (fill '𝜆')) v
 
-handleTuiEvent :: (V.Vector (V.Vector RGB)) -> BrickEvent n e -> EventM n (Next (V.Vector (V.Vector RGB)))
+handleTuiEvent
+  :: (V.Vector (V.Vector RGB))
+  -> BrickEvent n e
+  -> EventM n (Next (V.Vector (V.Vector RGB)))
 handleTuiEvent s e =
   case e of
     VtyEvent vtye ->
@@ -42,6 +61,6 @@ handleTuiEvent s e =
 main :: IO ()
 main = do
   initialState <- generateColors
-  endState <- defaultMain app initialState
-  print "end"
+  _ <- defaultMain (app $ colorMap initialState) initialState
+  putStrLn "end"
 
